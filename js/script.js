b94d39f4d4e162b2e826aebe9b6d7e26609e52fb
baimuhtar <baimuhtar1997@gmail.com>
2022-10-25 22:33:09 +0600

    Dark mode

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ let navbar = document.querySelector('.navbar');
 menu.onclick = () => {
     navbar.classList.toggle('active')
 }
+
+
+
+// dark Mode
+
+let darkmode = document.querySelector('#darkmode');
+
+darkmode.onclick = () =>{
+    if (darkmode.classList.contains('bx-moon')){
+        darkmode.classList.replace('bx-moon', 'bx-sun');
+        document.body.classList.add('active');
+    }
+    else {
+        darkmode.classList.replace('bx-sun', 'bx-moon');
+        document.body.classList.remove('active');
+    }
+}
